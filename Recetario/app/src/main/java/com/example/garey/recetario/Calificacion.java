50b53e6d64fbb1454b44b4956cf3d70a41687524
Cecy22 <37217264+Cecy22@users.noreply.github.com>
2018-05-13 19:20:16 -0500

    S3
    Vistas de la apk

---
+++ Recetario/app/src/main/java/com/example/garey/recetario/Calificacion.java
@@ -0,0 +1,13 @@
+package com.example.garey.recetario;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Calificacion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calificacion);
+    }
+}
---
+++ Recetario/app/src/main/java/com/example/garey/recetario/NuevaR.java
@@ -0,0 +1,13 @@
+package com.example.garey.recetario;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NuevaR extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nueva_r);
+    }
+}
