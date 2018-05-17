7c2659b9a8127d8692bee4040e524a802f6d4d64
Cecy22 <37217264+Cecy22@users.noreply.github.com>
2018-05-17 11:01:00 -0500

    s3

--- Recetario/app/src/main/java/com/example/garey/recetario/Inicio.java
+++ Recetario/app/src/main/java/com/example/garey/recetario/Inicio.java
@@ -1,13 +1,30 @@
 package com.example.garey.recetario;
 
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.SearchView;
+import android.widget.Toast;
+import android.widget.Toolbar;
 
 public class Inicio extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
+        //getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
     }
-}
+
+  }
+
+
--- Recetario/app/src/main/java/com/example/garey/recetario/MainActivity.java
+++ Recetario/app/src/main/java/com/example/garey/recetario/MainActivity.java
@@ -1,14 +1,26 @@
 package com.example.garey.recetario;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+Button in;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        in= (Button) findViewById(R.id.btnLogin);
     }
 
+    public void iniciar(View v){
+        Intent i = new Intent(this, Inicio.class);
+        startActivity(i);
+    }
+    public void registra(View v){
+        Intent i = new Intent(this, Registro.class);
+        startActivity(i);
+    }
 }
