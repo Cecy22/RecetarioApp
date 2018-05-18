e257216a9c38e6fc31a28581ca2183c8e5d52d50
Cecy22 <37217264+Cecy22@users.noreply.github.com>
2018-05-18 08:22:23 -0500

    s3

--- Recetario/app/src/main/java/com/example/garey/recetario/Inicio.java
+++ Recetario/app/src/main/java/com/example/garey/recetario/Inicio.java
@@ -1,30 +1,69 @@
 package com.example.garey.recetario;
 
-import android.os.Build;
-import android.support.annotation.RequiresApi;
-import android.support.v4.view.MenuItemCompat;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.WindowManager;
-import android.widget.SearchView;
 import android.widget.Toast;
-import android.widget.Toolbar;
 
-public class Inicio extends AppCompatActivity {
 
+public class Inicio extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    public Toolbar toolbar;
+    public TabLayout tabLayout;
+    public ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
         //getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        toolbar=(Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(true);
+
+        DrawerLayout drawer =(DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle= new ActionBarDrawerToggle(
+                this,drawer,toolbar, R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView=(NavigationView) findViewById(R.id.navi);
+        navigationView.setNavigationItemSelectedListener(this);
 
     }
 
-  }
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id= item.getItemId();
+        Toast.makeText(this, "hola", Toast.LENGTH_SHORT).show();
+
+        if(id==R.id.action_buscar){
+            Toast.makeText(this, "Buscar", Toast.LENGTH_SHORT).show();
+        }else if(id==R.id.action_Datos){
+            Intent i= new Intent(this, DatosUsuario.class);
+            startActivity(i);
+        }else if(id==R.id.action_Inicio){
+            Intent i = new Intent(this, Inicio.class);
+            startActivity(i);
+        }else if(id== R.id.action_Nuevo){
+            Intent i = new Intent(this, NuevaR.class);
+            startActivity(i);
+        }
+        DrawerLayout drawer =(DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
 
 
